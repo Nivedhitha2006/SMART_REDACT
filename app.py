e4dae8951868f25945f630ee23d7de3ba6593e0a
NIVEDHITHA K <nivedhi2006@gmail.com>
2026-04-29 20:17:16 +0530

    Fix: bind to PORT env variable, fix gunicorn start command

--- app.py
+++ app.py
@@ -127,5 +127,5 @@ def history():
     return jsonify(sessions)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
